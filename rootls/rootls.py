b876683694d11628c19957c3abe7f69f3784ca26
Florian Eble <florian.eble@cern.ch>
2021-03-08 08:18:37 +0100

    Revert "uproot 3 -> 4"
    This reverts commit 3e23aac8298bef1ec7990e6e60bd1a79607d9a8c.

--- rootls/rootls.py
+++ rootls/rootls.py
@@ -59,7 +59,7 @@ if (__name__ == "__main__"):
     if depth>0 and hasattr(file_, "keys"):
         for in1 in file_.keys():
             type_ = getType(type(file_[in1]))
-            element = list2str(in1.split(";")[:-1])
+            element = list2str(in1.decode("utf-8").split(";")[:-1])
             if args.nobold:
                 print(newline + type_ + "\t" + element )
             else:
@@ -69,12 +69,13 @@ if (__name__ == "__main__"):
                 if hasattr(f1, "keys"):
                     for in2 in f1.keys():
                         type_ = getType(type(f1[in2]))
-                        print(4*" " + type_ + "\t" + in2)
+                        #type_ = getType(type(file_[in1][in2]))
+                        print(4*" " + type_ + "\t" + in2.decode("utf-8"))
                         if depth>2:
                             f2 = f1[in2]
                             if hasattr(f2, "keys"):
                                 for in3 in f2.keys():
-                                    print(8*" " + in3)
+                                    print(8*" " + in3.decode("utf-8"))
 
 
 
