19ed4cd14a2677b7173a88c6d193d861ebc6ac40
Florian Eble <florian.eble@cern.ch>
2023-06-08 10:18:27 +0200

    Using helper script

--- cathead/cathead.py
+++ cathead/cathead.py
@@ -1,11 +1,8 @@
 import argparse
 import math
 import sys
-import os
 
-utils_directory = os.path.dirname(__file__)
-sys.path.append(utils_directory + "/../utils/")
-import utils
+from helpers.generalUtilities import run_bash_command
 
 
 if __name__ == "__main__":
@@ -16,22 +13,23 @@ if __name__ == "__main__":
         "-f", "--file",
         help="File to cat",
         required=True,
-        )
+    )
     parser.add_argument(
         "-p", "--percentage",
         help="Fraction of the file to cat, e.g. 5 means 5 percents",
         required=True,
         type=int
-        )
+    )
 
     args = parser.parse_args()
 
 
     # Number of line in the file to cat
-    n_lines_in_file = int(utils.run_bash_command("cat %s | wc -l" %(args.file)))
+    n_lines_in_file = int(run_bash_command("cat %s | wc -l" %(args.file)))
 
     # Number of lines to print
     n_lines_to_print = int(round(n_lines_in_file * args.percentage/100))
 
     # Print first lines
-    print(utils.run_bash_command("cat %s | head -n %d" %(args.file, n_lines_to_print)))
+    print(run_bash_command("cat %s | head -n %d" %(args.file, n_lines_to_print)))
+
