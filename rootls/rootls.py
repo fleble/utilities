24ee1861d0e694fac4da559d5405bd09eff41aa6
Florian Eble <florian.eble@cern.ch>
2023-06-08 10:18:27 +0200

    Using helper script and PEP8 compliant names

--- rootls/rootls.py
+++ rootls/rootls.py
@@ -1,24 +1,11 @@
 import uproot3
 import argparse
 
-
-def list2str(L, strForConcatenation=""):
-    """
-    Takes a list of elements convertible to str and optionally a string.
-    Returns the concatenation of elements in the list separated by the optional string.
-
-    """
-
-    if len(L)==0:
-        return("")
-    else:
-        s = L[0]
-        for el in L[1:]: s = s + strForConcatenation + str(el)
-        return(s)
+from helpers.generalUtilities import list_to_str
 
 
-def getType(fullType):
-    return(str(fullType).split("'")[1].split(".")[-1])
+def get_type(full_type):
+    return(str(full_type).split("'")[1].split(".")[-1])
 
 
 
@@ -37,12 +24,12 @@ if (__name__ == "__main__"):
         default=2,
         )
     parser.add_argument(
-        "-nb", "--nobold",
+        "-nb", "--no_bold",
         help="Do not use bold",
         action="store_true"
         )
     parser.add_argument(
-        "-nel", "--noemptyline",
+        "-nel", "--no_empty_line",
         help="No empty lines",
         action="store_true"
         )
@@ -55,15 +42,15 @@ if (__name__ == "__main__"):
 
     file_ = uproot3.open(filename)
 
-    if args.noemptyline: newline = ""
+    if args.no_empty_line: newline = ""
     else: newline = "\n"
 
     if depth>0 and hasattr(file_, "keys"):
         for in1 in file_.keys():
             in1 = in1.decode("utf-8")
             f1 = file_[in1]
-            type_ = getType(type(f1))
-            element = list2str(in1.split(";")[:-1])
+            type_ = get_type(type(f1))
+            element = list_to_str(in1.split(";")[:-1])
             if type_ == "TTree":
                 k0 = list(f1.keys())[0]
                 nevts = len(f1[k0])
@@ -74,7 +61,7 @@ if (__name__ == "__main__"):
                 nevtsStr = " (%d %s)" %(nevts, name)
             else:
                 nevtsStr = ""
-            if args.nobold:
+            if args.no_bold:
                 print(newline + type_ + "\t" + element + nevtsStr )
             else:
                 print("\033[1m" + newline + type_ + "\t" + element + nevtsStr + "\033[0m")
@@ -82,7 +69,7 @@ if (__name__ == "__main__"):
                 if hasattr(f1, "keys"):
                     for in2 in f1.keys():
                         in2 = in2.decode("utf-8")
-                        type_ = getType(type(f1[in2]))
+                        type_ = get_type(type(f1[in2]))
                         print(4*" " + type_ + "\t" + in2)
                         if depth>2:
                             f2 = f1[in2]
@@ -90,6 +77,4 @@ if (__name__ == "__main__"):
                                 for in3 in f2.keys():
                                     in3 = in3.decode("utf-8")
                                     print(8*" " + in3)
-
-
 
